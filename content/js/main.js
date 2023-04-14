bb01a11121c2f09de93d4635998a774fe5e00a00
JonanthaW <jonanthawillian@gmail.com>
2023-04-14 12:58:17 -0300

    CHANGED: Script now can look for other types of files

--- content/js/main.js
+++ content/js/main.js
@@ -1,30 +1,48 @@
 document.querySelector("body button").addEventListener("click", () => {
-  createURL();
+  document.querySelector("body").removeChild(document.querySelector("body button"))
+  createURL(100);
 })
 
-let imagesQuantity = 100;
+var extensionNum = 0;
 
-function createURL() {
+function createURL(imagesQuantity) {
     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < imagesQuantity; i++) { // begin creating URL of imgur
+    for (var i = 0; i < imagesQuantity; i++) {
       let url = "";
-      for (var l = 0; l <= 4; l++) {
-        url += alphabet.charAt(Math.floor(Math.random() * alphabet.length)); // innerjoin 5 characters randomly
+      for (var l = 0; l < 5; l++) {
+        url += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
       }
+      TryImage(url);
+    }
+}
+
+function TryImage(url) {
+      let oldURL = url;
+      let extensions = [".png", ".jpg", ".jpeg", ".gif", ".jpeg", ".svg", ".webp", ".ico", ".TIFF", ".APNG"];
+
       let img = new Image();
-      img.src = "http://i.imgur.com/" + url + ".jpg"; // join it all with imgur
+      img.src = `http://i.imgur.com/${url}${extensions[extensionNum]}`;
+
       img.onload = function() {
-        if (this.naturalWidth != 161 && !this.naturalHeight != 81) { // check if images exists
-          let content = document.createElement("img"); // if the image exists, create it
+        if (this.naturalWidth != 161 && !this.naturalHeight != 81) {
+          let content = document.createElement("img");
           content.src = img.src;
-          let a = document.createElement("a"); // click on the image to open it on another tab
-          a.href = img.src;
+          let a = document.createElement("a");
+          a.href = `http://i.imgur.com/${url}.png`;
           a.target = "_blank";
           a.referrer = "noreferrer";
-          a.append(content); // add it to the page
+          a.append(content);
           document.querySelector(".template").append(a);
         }
+        else {
+          extensionNum += 1;
+          if(extensionNum == extensions.length) {
+            extensionNum = 0;
+            createURL(50);
+          }
+          else {
+            TryImage(oldURL);
+          }
+        }
       }
-    }
-    document.querySelector("body button").innerText = "LOAD MORE";
 }
