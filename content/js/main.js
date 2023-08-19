ffe44056613b01382ee71606d7da9dbd648bdfa8
JonanthaW <jonanthawillian@gmail.com>
2023-08-19 01:56:38 -0300

    Rewritten code

--- content/js/main.js
+++ content/js/main.js
@@ -1,48 +1,56 @@
-document.querySelector("body button").addEventListener("click", () => {
-  document.querySelector("body").removeChild(document.querySelector("body button"))
-  createURL(100);
-})
+const extensions = [".png", ".jpg", ".jpeg", ".gif", ".svg", ".webp", ".ico", ".tiff", ".apng"];
 
-var extensionNum = 0;
+document.querySelector("body button").addEventListener("click", handleButtonClick);
+
+let extensionNum = 0;
+
+function handleButtonClick() {
+  const button = document.querySelector("body button");
+  button.removeEventListener("click", handleButtonClick);
+  button.parentNode.removeChild(button);
+  createURL(100);
+}
 
 function createURL(imagesQuantity) {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < imagesQuantity; i++) {
-      let url = "";
-      for (var l = 0; l < 5; l++) {
-        url += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-      }
-      TryImage(url);
-    }
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < imagesQuantity; i++) {
+    const url = generateRandomString(alphabet, 5);
+    TryImage(url);
+  }
+}
+
+function generateRandomString(characters, length) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
 
 function TryImage(url) {
-      let oldURL = url;
-      let extensions = [".png", ".jpg", ".jpeg", ".gif", ".jpeg", ".svg", ".webp", ".ico", ".TIFF", ".APNG"];
+  const img = new Image();
+  img.src = `http://i.imgur.com/${url}${extensions[extensionNum]}`;
 
-      let img = new Image();
-      img.src = `http://i.imgur.com/${url}${extensions[extensionNum]}`;
+  img.onload = function () {
+    if (this.naturalWidth !== 161 || this.naturalHeight !== 81) {
+      const content = document.createElement("img");
+      content.src = img.src;
 
-      img.onload = function() {
-        if (this.naturalWidth != 161 && !this.naturalHeight != 81) {
-          let content = document.createElement("img");
-          content.src = img.src;
-          let a = document.createElement("a");
-          a.href = `http://i.imgur.com/${url}.png`;
-          a.target = "_blank";
-          a.referrer = "noreferrer";
-          a.append(content);
-          document.querySelector(".template").append(a);
-        }
-        else {
-          extensionNum += 1;
-          if(extensionNum == extensions.length) {
-            extensionNum = 0;
-            createURL(50);
-          }
-          else {
-            TryImage(oldURL);
-          }
-        }
+      const a = document.createElement("a");
+      a.href = `http://i.imgur.com/${url}.png`;
+      a.target = "_blank";
+      a.rel = "noreferrer";
+      a.appendChild(content);
+
+      document.querySelector(".template").appendChild(a);
+    } else {
+      extensionNum = (extensionNum + 1) % extensions.length;
+      if (extensionNum === 0) {
+        createURL(50);
+      } else {
+        TryImage(url);
       }
+    }
+  };
 }
